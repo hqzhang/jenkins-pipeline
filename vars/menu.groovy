bb759867d557ab595ed7c351839d9971bd6abd19
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-09-05 23:46:49 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -463,7 +463,7 @@ def getRollbackList(String refvar, String filter){
 }
 
 def buildScript(List ls,String dft){
-    def ret = {}
+    def ret = []
     ls.each { 
         if ( it.contains(dft)) { ret.add(0,it) } 
         else { ret.add(it) } }
