c389d99a085f477bd55727e999ee515725841c9d
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-09-09 16:25:00 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -564,7 +564,7 @@ def getSolutionBackup(String ref){
 def getRollBackScript(){}
 
 def saveSolutionBackup(String component,String solutionBackup){
-    println( "Enter saveSolutionBackup:$solutionBackup")
+    println( "Enter saveSolutionBackup:${solutionBackup}")
     def restAPIHub='https://api.github.com/repos/hqzhang/solution-repo'
     def base="${restAPIHub}/contents/release"
 
