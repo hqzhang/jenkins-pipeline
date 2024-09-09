a06102c320e1995c3db1f2d4626297a1c36a18f4
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-09-09 16:32:49 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -577,8 +577,7 @@ def saveSolutionBackup(String component,String solutionBackup){
     def msg="create file message"
     def token=getToken(githubtokenid)
     println "token=$token"
-    def cmd="curl -kls -w '%{http_code}' -H 'Authorization: Bearer ${token}' 
-         ${base}/${solutionBackup}?ref=mytest "
+    def cmd="curl -kls -w '%{http_code}' -H 'Authorization: Bearer ${token}' ${base}/${solutionBackup}?ref=mytest "
     println "cmd=$cmd"
     def out=commandExecute(cmd)
     println "out=$out"
