41a41587c96bd289eec21cf296bef42b84199893
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-09-09 22:46:37 -0400

    update

--- vars/Utils.groovy
+++ vars/Utils.groovy
@@ -3,7 +3,7 @@ def urlBaseGithub='https://github.com/hqzhang/'
 @groovy.transform.Field
 def githubtokenid='myjenkinspipelinekey'
 
-def getCloneGithub(String repoName, String repoBranch, String myDir=''){
+def gitCloneGithub(String repoName, String repoBranch, String myDir=''){
 
   String tmp=myDir
   if (!myDir?.trim()) { tmp = repoName}
