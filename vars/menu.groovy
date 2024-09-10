aac5af1418e1ce8cc7e576a5e3ee4ba54529dd78
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-09-10 00:28:58 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -616,8 +616,7 @@ def saveSolutionBackup(String solutionBackupPath){
         
         body=JsonOutput.toJson(JsonOutput.toJson(body))
         println "create body=$body"
-        //cmd="curl -kLs -X PUT -o /dev/null -w '%{http_code}' -H 'Authorization: Bearer ${token}' \
-        cmd="curl -kLs -X PUT -H 'Authorization: Bearer ${token}' \
+        cmd="curl -kLs -X PUT -o /dev/null -w '%{http_code}' -H 'Authorization: Bearer ${token}' \
             ${base}/${solutionBackup} --data ${body}"
         println "create cmd=$cmd"
         out=commandExecute(cmd).trim()
