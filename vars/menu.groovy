46973fba77e55410f3d6b2d8c0a13c01324d52a0
Hongqi Zhang <zhanghongqi@hotmail.com>
2024-09-13 12:01:05 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -546,7 +546,7 @@ def getContentInstant(String ref ){
     |import jenkins.model.Jenkins
     |def ret = ''
     |if ( ${ref} == null || ${ref}.isEmpty() ) { return null }
-    |if ( ${ref}.contains('INIT') ) { return "<textarea name='value' rows='10' cols='120' >\${msg}</textarea>" }
+    |if ( ${ref}.contains('INIT') ) { return "<textarea name='value' rows='10' cols='120' >${msg}</textarea>" }
     |try {
     |   def credential = CredentialsProvider.lookupCredentials(StandardUsernamePasswordCredentials.class,
     |                    Jenkins.instance,null,null).find { it.id == '${githubtokenid}' }
