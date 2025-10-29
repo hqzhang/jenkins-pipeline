e3252991d7b1e09b555bc3d823c1a4934b695d39
Hongqi Zhang <zhanghongqi@hotmail.com>
2025-10-28 20:40:25 -0400

    update

---
+++ vars/kubedeploy.groovy
@@ -0,0 +1,45 @@
+#!/usr/bin/env groovy
+@groovy.transform.Field
+def app=myhello-app
+@groovy.transform.Field
+def image=wavecloud/$app
+@groovy.transform.Field
+def port=8081
+@groovy.transform.Field
+def url=www.wavecloud.com
+@groovy.transform.Field
+def KUBECFG="/Users/hongqizhang/.kube/config"
+
+    # First create your service
+    "kubectl delete deploy $app".execute()
+    "kubectl create deployment $app --image=$image".execute()
+
+    "kubectl delete svc $app".execute()
+    "kubectl expose deployment $app --port=$port".execute()
+
+    # Then create ingress
+    "kubectl delete ing nginx-ingress".execute()
+    "kubectl create ingress nginx-ingress --rule="$url/=$app:$port".execute()
+
+    println "verify application"
+    println "⏳ Waiting for ${url} to become reachable..."
+    while (true) {
+        def proc = ["curl", "-s", "--head", "--fail", url].execute()
+        proc.waitFor()
+        if (proc.exitValue() == 0) break
+        print "."
+        sleep(interval * 1000)
+    }
+
+    println "\nChecking response from ${url}..."
+    def response = ["curl", "-s", url].execute().text
+    def matched = response.contains(result)
+
+    println "res1=" + (matched ? result : "")
+
+    if (matched) {
+        println "TEST PASS!"
+    } else {
+        println "TEST ERROR!"
+    }
+ 
