c398ea70b718221d84bf05b02af53852e19f0aac
Hongqi Zhang <zhanghongqi@hotmail.com>
2025-10-28 21:10:35 -0400

    update

--- vars/kubedeploy.groovy
+++ vars/kubedeploy.groovy
@@ -1,14 +1,15 @@
 #!/usr/bin/env groovy
 @groovy.transform.Field
-def app=myhello-app
+def app='myhello-app'
 @groovy.transform.Field
-def image=wavecloud/$app
+def image='wavecloud/$app'
 @groovy.transform.Field
-def port=8081
+def port='8081'
 @groovy.transform.Field
-def url=www.wavecloud.com
+def url='www.wavecloud.com'
 @groovy.transform.Field
-def KUBECFG="/Users/hongqizhang/.kube/config"
+def KUBECFG='/Users/hongqizhang/.kube/config'
+
 def call(){
     println("First create your deploy")
     "kubectl delete deploy $app".execute()
