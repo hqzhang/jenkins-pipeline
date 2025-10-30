d207b38b30020240d3c9394de4116eeaf472a926
Hongqi Zhang <zhanghongqi@hotmail.com>
2025-10-29 20:43:48 -0400

    update

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/env groovy
-
+def commandExecute(String cmd){
+    def out = new ProcessBuilder('sh','-c',cmd).redirectErrorStream(true).start().text
+    return out
+}
 def call(){
     def cmd="kubectl get nodes"
-    println new ProcessBuilder('sh','-c',cmd).redirectErrorStream(true).start().text
+    println commandExecute(cmd)
 }
