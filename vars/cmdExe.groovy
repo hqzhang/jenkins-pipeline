3aa26c4344c9d39b6626fbd2fab0606275b3b5c2
hongqi zhang <hongqizhang@hongqis-mbp.home>
2026-01-06 14:44:18 -0500

    update

--- vars/cmdExe.groovy
+++ vars/cmdExe.groovy
@@ -1,9 +1,10 @@
 def call(String cmd){
     println "Enter cmdExe()"
     ProcessBuilder pb = new ProcessBuilder('sh','-c',cmd);
+    Map<String, String> env = pb.environment();
     println "PATH=$PATH"
      // Set a completely new PATH
-    pb.environment().env.put("PATH", PATH);
+    env.put("PATH", PATH);
     println "PATH=$PATH"
     Process out = pb.redirectErrorStream(true).start().text;
     return process
